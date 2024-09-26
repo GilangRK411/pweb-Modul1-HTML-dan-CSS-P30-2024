f770306c4bdda014158e2cc8649e0c1a4f062697
blackcat <136048717+blackTopHatCat@users.noreply.github.com>
2024-09-26 14:16:47 +0000

    Footer and header but in php
    Turned into php. Could be added to inside any php files so no need to copaste html elements and link css files.

---
+++ footer.php
@@ -0,0 +1,21 @@
+<!-- add <?php include "./footer.html" ?> to the desired placement -->
+
+<!DOCTYPE html>
+<html>
+        <head>
+                <style>
+body{font-family:serif; margin:0}
+body > * {padding:8px 15px}
+footer{background:#fef9c3;}
+footer > h3{text-align:center}
+
+                </style>
+        </head>
+        <body>
+                <footer>
+                        <h3>>> <a href="contribute.html" class="#">Contribute</a> to help all future users >:1 <<
+                        <br /> BUT DO FOLLOW THE <a href="rules.html" class="#">RULES</a> !</h3>
+                </footer>
+        </body>
+</html>
+
